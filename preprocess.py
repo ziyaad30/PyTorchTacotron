af6534923ec967ec7c7b29ebea9c735cc85abde0
Ziyaad <xavierbaatjes@yahoo.com>
2023-08-01 09:34:12 +0200

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -3,14 +3,14 @@ import os
 from multiprocessing import cpu_count
 from tqdm import tqdm
 from datasets import ljspeech
+from hparams import hparams
 
-frame_length_ms=50
-frame_shift_ms=12.5
+
 
 
 def preprocess_ljspeech(args):
-  in_dir = os.path.join(args.base_dir, 'LJSpeech-1.1')
-  out_dir = os.path.join(args.base_dir, args.output)
+  in_dir = os.path.join(hparams.dataset_root, 'LJSpeech-1.1')
+  out_dir = os.path.join(hparams.dataset_root, args.output)
   os.makedirs(out_dir, exist_ok=True)
   metadata = ljspeech.build_from_path(in_dir, out_dir, args.num_workers, tqdm=tqdm)
   write_metadata(metadata, out_dir)
@@ -21,7 +21,7 @@ def write_metadata(metadata, out_dir):
     for m in metadata:
       f.write('|'.join([str(x) for x in m]) + '\n')
   frames = sum([m[2] for m in metadata])
-  hours = frames * frame_shift_ms / (3600 * 1000)
+  hours = frames * hparams.frame_shift_ms / (3600 * 1000)
   print('Wrote %d utterances, %d frames (%.2f hours)' % (len(metadata), frames, hours))
   print('Max input length:  %d' % max(len(m[3]) for m in metadata))
   print('Max output length: %d' % max(m[2] for m in metadata))
@@ -29,7 +29,6 @@ def write_metadata(metadata, out_dir):
 
 def main():
   parser = argparse.ArgumentParser()
-  parser.add_argument('--base_dir', default='Speech')
   parser.add_argument('--output', default='training')
   parser.add_argument('--num_workers', type=int, default=cpu_count())
   args = parser.parse_args()
