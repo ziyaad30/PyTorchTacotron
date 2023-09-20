ebf66e7755d05e9b5133266f38c2c94c70765d17
Ziyaad <xavierbaatjes@yahoo.com>
2023-09-20 17:51:43 +0200

    Update __init__.py

--- text/__init__.py
+++ text/__init__.py
@@ -12,8 +12,8 @@ if os.name == 'nt':
     EspeakWrapper.set_library(_ESPEAK_LIBRARY)
 
 # Mappings from symbol to numeric ID and vice versa:
-_symbol_to_id = {s: i for i, s in enumerate(symbols)}
-_id_to_symbol = {i: s for i, s in enumerate(symbols)}
+symbol_to_id = {s: i for i, s in enumerate(symbols)}
+id_to_symbol = {i: s for i, s in enumerate(symbols)}
 
 # Regular expression matching text enclosed in curly braces:
 _curly_re = re.compile(r'(.*?)\{(.+?)\}(.*)')
@@ -26,43 +26,49 @@ def phoneme_text(text):
 
 def phoneme_to_sequence(text):
     sequence = []
-    
-    sequence += _symbols_to_sequence(text)
+
+    for symbol in text:
+        if symbol in symbol_to_id.keys():
+            symbol_id = symbol_to_id[symbol]
+            sequence += [symbol_id]
+        else:
+            raise Exception(f"Sorry, symbol {symbol} not found!")
     
     # Append EOS token
-    sequence.append(_symbol_to_id['~'])
+    # sequence.append(symbol_to_id['~'])
 
     return sequence
 
 def text_to_sequence(text, cleaner_names):
     sequence = []
     
-    # First clean text
-    text = _clean_text(text, cleaner_names)
-    # print(text)
+    # Clean text
+    clean_text = _clean_text(text, cleaner_names)
+
     # Phonemize text
     text = phoneme_text(text)
-    print(text)
-
-    # Check for curly braces and treat their contents as ARPAbet:
-    sequence += _symbols_to_sequence(text)
+    
+    # Convert text to symbols
+    for symbol in text:
+        if symbol in symbol_to_id.keys():
+            symbol_id = symbol_to_id[symbol]
+            sequence += [symbol_id]
+        else:
+            raise Exception(f"Sorry, symbol {symbol} not found!")
 
     # Append EOS token
-    sequence.append(_symbol_to_id['~'])
+    # sequence.append(symbol_to_id['~'])
     return sequence
 
 
 def sequence_to_text(sequence):
-  '''Converts a sequence of IDs back to a string'''
-  result = ''
-  for symbol_id in sequence:
-    if symbol_id in _id_to_symbol:
-      s = _id_to_symbol[symbol_id]
-      # Enclose ARPAbet back in curly braces:
-      if len(s) > 1 and s[0] == '@':
-        s = '{%s}' % s[1:]
-      result += s
-  return result.replace('}{', ' ')
+    '''Converts a sequence of IDs back to a string'''
+    result = ''
+    for symbol_id in sequence:
+        if symbol_id in id_to_symbol:
+            s = id_to_symbol[symbol_id]
+            result += s
+    return result.replace('}{', ' ')
 
 
 def _clean_text(text, cleaner_names):
@@ -72,15 +78,3 @@ def _clean_text(text, cleaner_names):
       raise Exception('Unknown cleaner: %s' % name)
     text = cleaner(text)
   return text
-
-
-def _symbols_to_sequence(symbols):
-  return [_symbol_to_id[s] for s in symbols if _should_keep_symbol(s)]
-
-
-def _arpabet_to_sequence(text):
-  return _symbols_to_sequence(['@' + s for s in text.split()])
-
-
-def _should_keep_symbol(s):
-  return s in _symbol_to_id and s != '_' and s != '~'
